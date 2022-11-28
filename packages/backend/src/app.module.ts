f9ee7e163c683aa93ac75e074fac7e99367d3e40
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-11-28 09:57:36 +0000

    add event emitter in case we want to do subscriptions later

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { CacheModule, Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [CacheModule.register(), EventEmitterModule.forRoot()],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
