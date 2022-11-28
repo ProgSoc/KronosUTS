1a4c12a08fbcbaf7bfde21625799b32af8b426ab
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-11-28 09:49:18 +0000

    remove quotations

--- packages/frontend/src/pages/HelloWorld.tsx
+++ packages/frontend/src/pages/HelloWorld.tsx
@@ -3,7 +3,7 @@ import { useHelloWorld } from "../features/hello-world";
 const HelloWorld: React.FC = () => {
   const { data, isLoading, error } = useHelloWorld();
 
-  if (isLoading) return <span>"Loading..."</span>;
+  if (isLoading) return <span>Loading...</span>;
 
   if (error) return <span>An error has occured: {error.message}</span>;
 
