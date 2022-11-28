de8e76191c86fbecd3321dca56c0082ff2db1a09
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-11-28 09:14:55 +0000

    add swagger

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,6 +1,7 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,15 @@ async function bootstrap() {
     type: VersioningType.URI,
     prefix: 'v',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Kronos API Docs')
+    .setDescription('The ProgSoc Kronos Timetable API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
