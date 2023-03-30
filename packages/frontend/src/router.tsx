ff8e86e8247208e1468ef0b6902336957ff523ca
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2023-03-30 01:59:28 +0000

    work plz

--- packages/frontend/src/router.tsx
+++ packages/frontend/src/router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import HelloWorld from "./pages/HelloWorld";
+import Timetable from "./pages/Timetable";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HelloWorld />,
+    element: <Timetable />,
   },
 ]);
 
