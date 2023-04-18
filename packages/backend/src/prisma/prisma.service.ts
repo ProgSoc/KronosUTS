1990a7d03f437114a64296e34df85ef5ba00ea50
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2023-04-18 12:01:40 +1000

    primsa

--- packages/backend/src/prisma/prisma.service.ts
+++ packages/backend/src/prisma/prisma.service.ts
@@ -4,26 +4,12 @@ import {
   Logger,
   OnModuleInit,
 } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly logger = new Logger(PrismaService.name),
-  ) {
-    const url = configService.getOrThrow('DATABASE_URL');
-
-    logger.debug({ url });
-
-    super({
-      datasources: {
-        db: {
-          url,
-        },
-      },
-    });
+  constructor(private readonly logger = new Logger(PrismaService.name)) {
+    super();
   }
 
   async onModuleInit() {
